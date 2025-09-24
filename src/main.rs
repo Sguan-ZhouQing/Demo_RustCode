23b94b5c5f60694bf84eb57aa73a762933fa7dd0
星必尘Sguan <148698322+Sguan-ZhouQing@users.noreply.github.com>
2025-09-24 18:06:04 +0800

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+/*
+ * @Author: 星必尘Sguan
+ * @Date: 2025-09-19 17:06:46
+ * @LastEditors: 星必尘Sguan|3464647102@qq.com
+ * @LastEditTime: 2025-09-20 12:41:20
+ * @FilePath: \Demo_RustCode\src\main.rs
+ * @Description: 
+ * 
+ * Copyright (c) 2025 by $JUST, All Rights Reserved. 
+ */
 fn main() {
     println!("Hello, world!");
+    #[allow(unused_variables)]
+    let my_arr = [10,20,30,40,50];
+
 }
+
+
+// fn my_own(x: i32,y: u8) -> u32{
+//     let mut arr: [i32,5] = [10,20,30,40,50];
+
+// }
